b3851fd1ab8047dbb4d64974d19b1370ed063781
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2021-05-04 12:46:43 +0300

    Initial implementation of store with ADD and Remove launch pads

---
+++ src/store/globalState.js
@@ -0,0 +1,52 @@
+import React, {createContext, useReducer, useEffect} from "react";
+import Reducer from "./reducer";
+
+/* Setup the initial state of the context, local storage
+will be used to keep track of the favorite items of the users
+
+*/
+const initialState = {
+  userFavoriteLaunches: localStorage.getItem("userFavoriteLaunches") ?
+  JSON.parse (localStorage.getItem(userFavoriteLaunches)) : [],
+
+  userFavoriteLaunchPads: localStorage.getItem("userFavoriteLaunchPads") ? JSON.parse (localStorage.getItem(userFavoriteLaunches)) : [],
+}
+
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = (props) => {
+  const [state, dispatch] = useReducer(Reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "userFavoriteLaunches",
+      JSON.stringify(state.userFavoriteLaunches)
+    );
+    localStorage.setItem(
+      "userFavoriteLaunchPads",
+       JSON.stringify(state.userFavoriteLaunchPads)
+    );
+  }, [state]);
+
+  const addItemToFavoriteLaunchPads = (item) => {
+    dispatch({ type: "ADD_ITEM_TO_FAVORITE_LAUNCH_PADS", payload: item });
+  }
+
+  const removeItemFromFavoriteLaunchPads = (item) => {
+    dispatch({ type: "REMOVE_ITEM_FROM_FAVORITE_LAUNCH_PADS", payload: item.site_id });
+  };
+
+  return (
+    <GlobalContext.Provider
+      value = {{
+        userFavoriteLaunchPads: state.userFavoriteLaunchPads,
+        userFavoriteLaunches: state.userFavoriteLaunches,
+        addItemToFavoriteLaunchPads,
+        removeItemFromFavoriteLaunchPads
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
